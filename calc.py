cc8c76d162c3797ec9f65701f0f8b089f12cfdff
Pimkach <kososes@gmail.com>
2024-09-08 17:32:23 +0600

    update

--- calc.py
+++ calc.py
@@ -1,90 +1,119 @@
-V = None
-A = None
+main_cycle = True
+num1 = None
 result = None
 
-while V != 'Stop':
+def add(num1, num2):
+    """сложение"""
+    return num1 + num2
 
-    print('''Доступные операции:
-          сложение; вычитание; умножение; деление; возведение в степень; вычисление остатка от деления.''')
+def subtract(num1, num2):
+    """вычитание"""
+    return num1 - num2
 
+def multiply(num1, num2):
+    """умножение"""
+    return num1 * num2
+
+def divide(num1, num2):
+    """
+    деление
+    ValueError при попытке деления на ноль
+    """
+    if num2 == 0:
+        raise ValueError("Деление на ноль невозможно")
+    return num1 / num2
+
+def power(a, b):
+    """возводит в степень"""
+    return a ** b
+
+def modulo(a, b):
+    """остаток от деления"""
+    return a % b
+
+def perform_operation(operator, num1, num2):
+    """выполнение арифметической операции"""
+    operations = {
+        '+': add,
+        '-': subtract,
+        '*': multiply,
+        '/': divide,
+        '^': power,
+        '%': modulo
+    }
+
+    if operator not in operations:
+        raise ValueError("Неподдерживаемая операция")
+
+    return operations[operator](num1, num2)
+
+def round_result(result):
+    """
+    запрашивает, нужно ли округлить результат,
+    и если да, то до какого количества знаков после запятой
+    """
     while True:
-        operator = input('Выберите операцию (+ - * / ^ %): ')
-        if operator in ['+', '-', '*', '/', '^', '%']:
-            break
-        else:
-            print("Некорректный оператор. Попробуйте снова.")
-
-
-    if A is None:
-        while True:
-            try:
-                A = float(input("Введите первое число: "))
-                break
-            except ValueError:
-                print("Некорректный ввод. Пожалуйста, введите число.")
-
-    while True:
-        try:
-            B = float(input("Введите второе число: "))
-            break
-        except ValueError:
-            print("Некорректный ввод. Пожалуйста, введите число.")
-
-
-    if operator == '+':
-        result = A + B
-    elif operator == '-':
-        result = A - B
-    elif operator == '*':
-        result = A * B
-    elif operator == '/':
-        if B == 0:
-            print('Нельзя делить на ноль. Попробуйте заново')
-            continue
-        else:
-            result = A / B
-    elif operator == '^':
-        result = A ** B
-    elif operator == '%':
-        result = A % B
-
-    while True:
-        znak = input('Хотите задать количество знаков после запятой? Да/Нет: ')
-        if znak.lower() in ['да', 'нет']:
+        want_round = input('Хотите задать количество знаков после запятой? Да/Нет: ')
+        if want_round.lower() in ['да', 'нет']:
             break
         else:
             print("Пожалуйста, введите 'Да' или 'Нет'.")
 
-    if znak.lower() == 'да':
+    if want_round.lower() == 'да':
         while True:
             try:
                 col = int(input('Сколько вы хотите знаков после запятой? Введите цифру: '))
                 result = round(result, col)
-                break
+                return result
             except ValueError:
                 print("Пожалуйста, введите целое число.")
+    else:
+        return result
 
-
-    print(result)
-
+def get_user_input(prompt, valid_options=None):
+    """запрашивает ввод и проверяет его корректность"""
     while True:
-        W = input('Вы хотите продолжить вычисления с полученным результатом? Да/Нет: ')
-        if W.lower() in ['да', 'нет']:
-            break
+        user_input = input(prompt)
+        if valid_options is None:
+            try:
+                return float(user_input)
+            except ValueError:
+                print("Некорректный ввод. Пожалуйста, введите число.")
+        elif user_input in valid_options:
+            return user_input
         else:
-            print("Пожалуйста, введите 'Да' или 'Нет'.")
+            print(f"Некорректный ввод. Допустимые варианты: {', '.join(valid_options)}")
 
-    if W.lower() == 'да':
-        A = result
-    elif W.lower() == 'нет':
-        A = None
 
-    while True:
-        check = input('Хотите продолжить или выйти из программы? Продолжить/Выйти: ')
-        if check.lower() in ['продолжить', 'выйти']:
-            break
-        else:
-            print("Пожалуйста, введите 'Продолжить' или 'Выйти'.")
+while main_cycle:
 
-    if check.lower() == 'выйти':
-        V = 'Stop'
+    if num1 is None:
+        num1 = get_user_input("Введите первое число: ")
+
+    print('''Доступные операции:
+          сложение; вычитание; умножение; деление; возведение в степень; вычисление остатка от деления.''')
+
+    try:
+        operator = get_user_input("Выберите операцию (+ - * / ^ %): ", ['+', '-', '*', '/', '^', '%'])
+
+        num2 = get_user_input("Введите второе число: ")
+
+        result = perform_operation(operator, num1, num2)
+        rounded_result = round_result(result)
+        print(rounded_result)
+
+        answer = get_user_input("Хотите продолжить вычисления с полученным результатом? (+/-): ", ['+', '-'])
+
+        if answer.lower() == '+':
+            num1 = rounded_result
+        elif answer.lower() == '-':
+            num1 = None
+    except ValueError as e:
+        print(f"Ошибка: {e}")
+        num1 = None
+
+
+    answer = get_user_input("Хотите продолжить или выйти из программы? (+/-): ", ['+', '-'])
+
+    if answer == '-':
+        main_cycle = False
